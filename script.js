fce86b4a05adfd9700584b24e3490459876ef701
silanurs <sarii.silanur@gmail.com>
2023-04-30 22:07:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const display = document.getElementById('display');
+const output = document.getElementById('output');
+const buttons = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operation');
+const clear = document.getElementById('clear');
+const deletion = document.getElementById('delete');
+const decimal = document.querySelector('.decimal')
+
+
+
