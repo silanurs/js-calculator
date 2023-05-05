e0647c7aa77c7306f36a6ed3804b8e9e3469fef5
silanurs <sarii.silanur@gmail.com>
2023-05-05 16:23:39 +0300

    applied the calculator logic

--- script.js
+++ script.js
@@ -1,10 +1,131 @@
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+let displayValue = "0";
+let decimalEntered = false;
+
+
+
 const display = document.getElementById('display');
 const output = document.getElementById('output');
-const buttons = document.querySelectorAll('.number');
+const buttons = document.querySelectorAll('button');
+const equals = document.getElementById('equals');
+const decimal = document.querySelector('.decimal');
 const operators = document.querySelectorAll('.operation');
-const clear = document.getElementById('clear');
+const clear = document.getElementById('clearAll');
 const deletion = document.getElementById('delete');
-const decimal = document.querySelector('.decimal')
+const numbers = document.querySelector('.number')
 
+output.textContent=displayValue;
+operators.forEach(operator=> operator.addEventListener('click', setOperator));
+buttons.forEach(btn =>{ btn.addEventListener('click', handleClick)})
+// Define functions for basic arithmetic operations
+function add(a, b) {
+    return a + b;
+  }
+  
+function subtract(a, b) {
+    return a - b;
+  }
+  
+function multiply(a, b) {
+    return a * b;
+  }
+  
+ function divide(a, b) {
+    if (b === 0) {
+      return null; // Handle division by zero
+    }
+    return a / b;
+  }
+  
+function handleClick(e){
+  
+  if(e.target.className==="number" && operator === null){
+    if(displayValue==="0"){
+      displayValue="";
+    }
+    displayValue+=e.target.innerText
+    display.textContent += e.target.innerText;
+    output.textContent=displayValue
+    firstNumber=parseFloat(displayValue)
+  }
+  if(e.target.className==="number" && operator !== null){
+    if( display.textContent.endsWith(operator)){
+      displayValue=""
+    }
+    displayValue+=e.target.innerText
+    display.textContent+= e.target.innerText;
+    output.textContent=displayValue;
+    secondNumber=parseFloat(displayValue)
+  }
+  if(e.target.id==="clearAll"){
+    reset()
+  }
+  if(e.target.id==="delete"){
+    display.textContent=display.textContent.slice(0,-1);
+    output.textContent=output.textContent.slice(0, -1);
+    displayValue=displayValue.slice(0, -1)
+  }
+  if(e.target.id==="equals"){
+    display.textContent+= "="
+    operate(operator);
 
+  }
+}
 
+function setOperator(e){
+  if(e.target.innerText==="+"){
+    operator = "+";
+    output.textContent=operator;
+    display.textContent+=operator
+  }
+  if(e.target.innerText==="-"){
+    operator= "-";
+    display.textContent+=operator
+    output.textContent=operator;
+  }
+  if(e.target.innerText==="*"){
+    operator= "*";
+    display.textContent+=operator
+    output.textContent=operator;
+  }
+  if(e.target.innerText==="/"){
+    operator = "/";
+    display.textContent+=operator
+    output.textContent=operator;
+  }
+}
+function operate(operator){
+  let result = null;
+  switch(operator){
+    case "+":
+      result = add(firstNumber, secondNumber);
+      output.textContent=result;
+      break;
+    case "-":
+      result= subtract(firstNumber, secondNumber);
+      output.textContent=result;
+      break;
+    case "*":
+      result=multiply(firstNumber, secondNumber);
+      output.textContent=result;
+      break;
+    case "/":
+      result= divide(firstNumber, secondNumber);
+      output.textContent=result;
+      break;
+  }
+  return result;
+}
+
+function reset(){
+   firstNumber = null;
+   operator = null;
+   secondNumber = null;
+   displayValue = "0";
+   decimalEntered = false;
+   output.textContent=displayValue;
+   display.textContent=""
+  
+}
